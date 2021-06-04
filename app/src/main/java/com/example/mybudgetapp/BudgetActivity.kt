e37b849d9595653638255060b072d97983ba47b8
Brendon Marques <4thmarques@gmail.com>
2021-06-04 08:37:37 -0600

    Added extra features.

--- app/src/main/java/com/example/mybudgetapp/BudgetActivity.kt
+++ app/src/main/java/com/example/mybudgetapp/BudgetActivity.kt
@@ -7,7 +7,8 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class BudgetActivity : AppCompatActivity() {
@@ -23,23 +24,35 @@ class BudgetActivity : AppCompatActivity() {
         val tvMessage : TextView = findViewById(R.id.tv_period)
         tvMessage.text = "Period: $month/$year"
 
-        val db = FirebaseFirestore.getInstance()
+        val db = Firebase.firestore
         val data = AppData()
         data.month = month
         data.year = year
         val databaseFunctions = AppDatabase()
 
         databaseFunctions.retrieveAllDocuments(db, data)
-        println("incomedata ${data.incomesDb} month/year ${data.month} / ${data.year}")
+        setUpData(data)
 
+    }
+
+    fun setUpData(data: AppData) {
         val lvIncome: ListView = findViewById(R.id.lv_income)
         val textViewResult: TextView = findViewById(R.id.tv_result)
+        if (data.incomesDb.size < 1) {
+            val message = arrayOf("No incomes")
 
-        val adapter: ArrayAdapter<Income> = ArrayAdapter(
-            this,
-            android.R.layout.simple_dropdown_item_1line,data.incomesDb
-        )
-        lvIncome.adapter = adapter
+            val adapter: ArrayAdapter<String> = ArrayAdapter(
+                this,
+                android.R.layout.simple_dropdown_item_1line,message
+            )
+            lvIncome.adapter = adapter
+        } else {
+            val adapter: ArrayAdapter<Income> = ArrayAdapter(
+                this,
+                android.R.layout.simple_dropdown_item_1line,data.incomesDb
+            )
+            lvIncome.adapter = adapter
+        }
 
         lvIncome.onItemClickListener =
             AdapterView.OnItemClickListener { parent, view, position, id ->
--- app/src/main/java/com/example/mybudgetapp/Database.kt
+++ app/src/main/java/com/example/mybudgetapp/Database.kt
@@ -1,17 +1,21 @@
 package com.example.mybudgetapp
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
 
-class AppDatabase {
+
+open class AppDatabase {
     // Push an income to the database
     fun addIncomeDb(db: FirebaseFirestore, income: Income, data: AppData) {
         val incomeDb = HashMap<String, Any>()
         incomeDb["source"] = income.source
         incomeDb["value"] = income.value
-        db.collection("${data.year}").document("${data.month}").collection("income").document().set(incomeDb)
+        db.collection("${data.year}").document("${data.month}").collection("income").document()
+            .set(incomeDb)
     }
 
     // Push an expense to the database
@@ -20,14 +24,16 @@ class AppDatabase {
         expenseDb["source"] = expense.source
         expenseDb["value"] = expense.value
         expenseDb["category"] = expense.categoryName
-        db.collection("${data.year}").document("${data.month}").collection("expense").document().set(expenseDb)
+        db.collection("${data.year}").document("${data.month}").collection("expense").document()
+            .set(expenseDb)
     }
 
     fun addCategoryDb(db: FirebaseFirestore, data: AppData, category: Category) {
         val categoryDb = HashMap<String, Any>()
         categoryDb["name"] = category.name
         categoryDb["budget"] = category.budget
-        db.collection("${data.year}").document("${data.month}").collection("categories").document().set(categoryDb)
+        db.collection("${data.year}").document("${data.month}").collection("categories").document()
+            .set(categoryDb)
     }
 
     // Edit an income in the database
@@ -42,20 +48,30 @@ class AppDatabase {
     fun editExpenseDb(db: FirebaseFirestore, data: AppData, index: Int, newAppData: Expense) {
         val docRef = db.collection("${data.year}").document("${data.month}").collection("expense")
             .document(data.expensesDbKeys[index - 1])
-        docRef.update("source", newAppData.source, "value", newAppData.value, "category", newAppData.categoryName)
+        docRef.update(
+            "source",
+            newAppData.source,
+            "value",
+            newAppData.value,
+            "category",
+            newAppData.categoryName
+        )
         println("Expense edited.")
     }
 
     fun editCategoryDb(db: FirebaseFirestore, data: AppData, index: Int, category: Category) {
-        val docRef = db.collection("${data.year}").document("${data.month}").collection("categories").document(data.categoriesId[index - 1])
+        val docRef =
+            db.collection("${data.year}").document("${data.month}").collection("categories")
+                .document(data.categoriesId[index - 1])
         docRef.update("name", category.name, "budget", category.budget)
 
         // This code will edit the category for the existing expenses entries
         val newCategory = data.categories[index - 1].name
         for ((idx, value) in data.expensesDb.withIndex()) {
             if (value.categoryName == newCategory) {
-                val docRefExisting = db.collection("${data.year}").document("${data.month}").collection("expense")
-                    .document(data.expensesDbKeys[idx])
+                val docRefExisting =
+                    db.collection("${data.year}").document("${data.month}").collection("expense")
+                        .document(data.expensesDbKeys[idx])
                 docRefExisting.update("category", newCategory)
             }
         }
@@ -81,12 +97,14 @@ class AppDatabase {
         // This code will delete the category for the existing expenses entries
         for ((idx, value) in data.expensesDb.withIndex()) {
             if (value.categoryName == data.categories[index - 1].name) {
-                val docRef = db.collection("${data.year}").document("${data.month}").collection("expense")
-                    .document(data.expensesDbKeys[idx])
+                val docRef =
+                    db.collection("${data.year}").document("${data.month}").collection("expense")
+                        .document(data.expensesDbKeys[idx])
                 docRef.update("category", "")
             }
         }
-        db.collection("${data.year}").document("${data.month}").collection("categories").document(data.categoriesId[index - 1]).delete()
+        db.collection("${data.year}").document("${data.month}").collection("categories")
+            .document(data.categoriesId[index - 1]).delete()
         println("Category deleted.")
     }
 
@@ -156,73 +174,117 @@ class AppDatabase {
     // Loop through the income and expense collections in the database, and store the data in local variables.
     // The 'onEvent' function will listen to any changes on the database, and automatically retrieve the changed data.
     fun retrieveAllDocuments(db: FirebaseFirestore?, data: AppData) {
-        println("made it to retrieve all")
+
+//        val docRef = db?.collection("${data.year}")?.document("${data.month}")?.collection("income")
+//        docRef?.get()
+//            ?.addOnSuccessListener { document ->
+//                data.incomesDbKeys.clear()
+//                data.incomesDb.clear()
+//                println("is document null ${document == null}")
+//                if (document != null) {
+//                    println("DocumentSnapshot data: ${document.data}")
+//                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+//                } else {
+//                    Log.d(TAG, "No such document")
+//                }
+//            }
+//            ?.addOnFailureListener { exception ->
+//                Log.d(TAG, "get failed with ", exception)
+//            }
+
+//        db!!.collection("Hello")
+//            .get()
+//            .addOnSuccessListener { result ->
+//                println("success listening $result")
+//                for (document in result) {
+//                    println("id and data test ${document.id} => ${document.data}")
+//                    Log.d(TAG, "${document.id} => ${document.data}")
+//                }
+//            }
+//            .addOnFailureListener { exception ->
+//                println("failure listening")
+//                Log.d(TAG, "Error getting documents: ", exception)
+//            }
+
         db?.collection("${data.year}")?.document("${data.month}")?.collection("income")
-            ?.addSnapshotListener(object : EventListener<QuerySnapshot?> {
-                override fun onEvent(snapshots: QuerySnapshot?, e: FirebaseFirestoreException?) {
-                    data.incomesDbKeys.clear()
-                    data.incomesDb.clear()
-                    if (e != null) {
-                        println("Listen failed:$e")
-                        return
-                    }
-                    if (snapshots != null) {
-                        for (doc in snapshots) {
-                            doc.getString("source")?.let {
-                                doc.getDouble("value")
-                                    ?.let { it1 -> Income(it, it1) }
-                            }?.let { data.incomesDb.add(it) }
-                            data.incomesDbKeys.add(doc.id)
+            ?.addSnapshotListener(
+                object : EventListener<QuerySnapshot?> {
+                    override fun onEvent(
+                        snapshots: QuerySnapshot?,
+                        e: FirebaseFirestoreException?
+                    ) {
+                        data.incomesDbKeys.clear()
+                        data.incomesDb.clear()
+                        if (e != null) {
+                            println("Listen failed:$e")
+                            return
                         }
-                        println("myincomes ${data.incomesDb}")
+                        if (snapshots != null) {
+                            for (doc in snapshots) {
+                                doc.getString("source")?.let {
+                                    doc.getDouble("value")
+                                        ?.let { it1 -> Income(it, it1) }
+                                }?.let { data.incomesDb.add(it) }
+                                data.incomesDbKeys.add(doc.id)
+                                println("added dataaaaaa retrieve")
+                            }
+
+                        }
                     }
-                }
-            })
+                })
 
         db?.collection("${data.year}")?.document("${data.month}")?.collection("expense")
-            ?.addSnapshotListener(object : EventListener<QuerySnapshot?> {
-                override fun onEvent(snapshots: QuerySnapshot?, e: FirebaseFirestoreException?) {
-                    data.expensesDb.clear()
-                    data.expensesDbKeys.clear()
-                    if (e != null) {
-                        println("Listen failed:$e")
-                        return
-                    }
-                    if (snapshots != null) {
-                        for (doc in snapshots) {
-                            doc.getString("source")?.let { source ->
-                                doc.getDouble("value")?.let { value ->
-                                    doc.getString("category")?.let { category ->
-                                        Expense(source, value, category)
+            ?.addSnapshotListener(
+                object : EventListener<QuerySnapshot?> {
+                    override fun onEvent(
+                        snapshots: QuerySnapshot?,
+                        e: FirebaseFirestoreException?
+                    ) {
+                        data.expensesDb.clear()
+                        data.expensesDbKeys.clear()
+                        if (e != null) {
+                            println("Listen failed:$e")
+                            return
+                        }
+                        if (snapshots != null) {
+                            for (doc in snapshots) {
+                                doc.getString("source")?.let { source ->
+                                    doc.getDouble("value")?.let { value ->
+                                        doc.getString("category")?.let { category ->
+                                            Expense(source, value, category)
+                                        }
                                     }
-                                }
-                            }?.let { data.expensesDb.add(it) }
-                            data.expensesDbKeys.add(doc.id)
+                                }?.let { data.expensesDb.add(it) }
+                                data.expensesDbKeys.add(doc.id)
+                            }
                         }
                     }
-                }
-            })
+                })
 
         db?.collection("${data.year}")?.document("${data.month}")?.collection("categories")
-            ?.addSnapshotListener(object : EventListener<QuerySnapshot?> {
-                override fun onEvent(snapshots: QuerySnapshot?, e: FirebaseFirestoreException?) {
-                    data.categories.clear()
-                    data.categoriesId.clear()
-                    if (e != null) {
-                        println("Listen failed:$e")
-                        return
-                    }
-                    if (snapshots != null) {
-                        for (doc in snapshots) {
-                            doc.getString("name")?.let { name ->
-                                doc.getDouble("budget")?.let { budget ->
-                                    Category(name, budget)
-                                }
-                            }?.let { data.categories.add(it) }
-                            data.categoriesId.add(doc.id)
+            ?.addSnapshotListener(
+                object : EventListener<QuerySnapshot?> {
+                    override fun onEvent(
+                        snapshots: QuerySnapshot?,
+                        e: FirebaseFirestoreException?
+                    ) {
+                        data.categories.clear()
+                        data.categoriesId.clear()
+                        if (e != null) {
+                            println("Listen failed:$e")
+                            return
+                        }
+                        if (snapshots != null) {
+                            for (doc in snapshots) {
+                                doc.getString("name")?.let { name ->
+                                    doc.getDouble("budget")?.let { budget ->
+                                        Category(name, budget)
+                                    }
+                                }?.let { data.categories.add(it) }
+                                data.categoriesId.add(doc.id)
+                            }
                         }
                     }
-                }
-            })
+                })
     }
 }
